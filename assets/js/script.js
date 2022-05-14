404884b07c80b32214b1753f0da2a3d23c7a2be4
Karina Montaldo <montaldo.k@gmail.com>
2022-05-13 20:44:52 -0400

    Prueba

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-
-//Scroll
+  //Scroll
 $(document).scroll( function(e){
   const y = $("html").scrollTop();
  if(y > 550) $("nav").addClass("nav-black")
    else $("nav").removeClass("nav-black")
 })
 
+document.querySelector('.navbar-toggler').addEventListener('click', () => {
+  document.querySelector('#menunav').classList.toggle('hide')
+})
 
 // TOOLTIP
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
